cfbe80bc6ee53be60e2f0041823e38cdd76be2be
cmwuaan <minhquanrbpk@gmail.com>
2024-05-06 11:21:17 +0700

    feat: setup react, vite, tailwindcss for project

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import classes from './Footer.module.css';
+
+function Footer() {
+  return (
+    <>
+      <footer className={classes.footer}>Footer</footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import classes from './Navbar.module.css';
+
+function Navbar() {
+  return <nav className={classes.navbar}>Navbar</nav>;
+}
+
+export default Navbar;
