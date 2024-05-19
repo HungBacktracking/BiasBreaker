32597b6a6c21af353fd3e8c0e463b4267d9d044e
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-05-20 03:24:14 +0700

    add insert keyword by LLM

--- crawler/LLM_Processing/Keyword.py
+++ crawler/LLM_Processing/Keyword.py
@@ -6,5 +6,8 @@ def insert_keywords(dataset):
         text = article['content']
         kw_extractor = KeywordExtractor()
         keywords = kw_extractor.extract_keywords(text)
+        for keyword in keywords:
+            if keyword not in article['keywords']:
+                article['keywords'].append(keyword)
         article['keywords'] = keywords
         time.sleep(15)
