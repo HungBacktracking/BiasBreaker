5f3f09a9f2b2801d0f8316905977854d543afc19
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-05-19 12:51:29 +0700

    feat: create some component for homepage screen

--- frontend/src/components/MainNews/Heading/Heading.jsx
+++ frontend/src/components/MainNews/Heading/Heading.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import classes from './Heading.module.css';
 
 const Heading = ( { title } ) => {
     return (
-        <div className="heading">
-            <h2>{title}</h2>
-            <i></i>
+        <div className={classes.heading}>
+            <h3 className={classes.heading_title}>{title}</h3>
+            <i className={`${classes.heading_icon} fa-solid fa-chevron-right`}></i>
         </div>
     )
 }
+
+export default Heading;
---
+++ frontend/src/components/TopArticleList/TopArticleList.jsx
@@ -0,0 +1,64 @@
+import classes from './TopArticleList.module.css';
+import { useState, useEffect } from 'react';
+import TopArticleItem from './TopArticleItem';
+
+const sampleData = [
+    { 
+      id: 1, 
+      title: "Iphone sắp thoát lối mòn Mobile", 
+      imagePath: "https://image.nhandan.vn/w800/Uploaded/2024/athlraguvhlra/2024_05_18/dai-tuong-to-lam-9610-81.jpg.webp", 
+      time: "Hôm nay", 
+      category: "Politics", 
+      content: "Content of article 1",
+      relatedArticles: [
+        { id: 4, title: "Related Article 1 for Article 1", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 1 for article 1" },
+        { id: 5, title: "Related Article 2 for Article 1", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 2 for article 1" },
+        { id: 6, title: "Related Article 3 for Article 1", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 3 for article 1" }
+      ]
+    },
+    { 
+      id: 2, 
+      title: "Iphone sắp thoát lối mòn Mobile", 
+      imagePath: "https://image.nhandan.vn/w800/Uploaded/2024/athlraguvhlra/2024_05_18/dai-tuong-to-lam-9610-81.jpg.webp", 
+      time: "Hôm nay", 
+      category: "Politics", 
+      content: "Content of article 2",
+      relatedArticles: [
+        { id: 7, title: "Related Article 1 for Article 2", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 1 for article 2" },
+        { id: 8, title: "Related Article 2 for Article 2", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 2 for article 2" },
+        { id: 9, title: "Related Article 3 for Article 2", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 3 for article 2" }
+      ]
+    },
+    { 
+      id: 3, 
+      title: "Iphone sắp thoát lối mòn Mobile", 
+      imagePath: "https://image.nhandan.vn/w800/Uploaded/2024/athlraguvhlra/2024_05_18/dai-tuong-to-lam-9610-81.jpg.webp", 
+      time: "Hôm nay", 
+      category: "Sports", 
+      content: "Content of article 3",
+      relatedArticles: [
+        { id: 10, title: "Related Article 1 for Article 3", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 1 for article 3" },
+        { id: 11, title: "Related Article 2 for Article 3", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 2 for article 3" },
+        { id: 12, title: "Related Article 3 for Article 3", imagePath: "https://via.placeholder.com/150", time: "Hôm qua", content: "Content of related article 3 for article 3" }
+      ]
+    }
+  ];
+
+const TopArticleList = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const filteredArticles = sampleData;
+        setArticles(filteredArticles);
+    }, [articles]);
+
+    return (
+        <>
+            {articles.map(article => (
+                <TopArticleItem article={article} />
+            ))}
+        </>
+    );
+}
+
+export default TopArticleList;
